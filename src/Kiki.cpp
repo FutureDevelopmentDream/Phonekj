966c64bacca2dcbe9daeb722e1a9bb58e1ae5c2a
DevLock <Ferchulin6489@Github.com>
2017-12-27 04:36:03 +0100

    1

--- src/Kiki.cpp
+++ src/Kiki.cpp
@@ -1,1 +1,30 @@
+#include<stdio.h>
+#include <WinSock2.h>
 
+
+
+void InitSock()
+{
+
+}
+
+void FinSock()
+{
+
+}
+
+int main_f_Sock()
+{
+	WSADATA wsa;
+
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		return 1;
+	}
+
+	printf("Initialised.");
+
+	return 0;
+}
